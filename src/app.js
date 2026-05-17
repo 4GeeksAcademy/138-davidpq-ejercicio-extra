748f526fe48efe6e56f62b9814250a4124eaed59
Xipxap11 <davidplanaquerol@gmail.com>
2026-05-17 08:39:43 +0000

    acabdo piedra papel tijera

--- src/app.js
+++ src/app.js
@@ -5,7 +5,38 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+let optionBot = ["Piedra", "Papel", "Tijera"]
+let player = window.prompt("Elige: Piedra, papel, tijera");
+let bot = optionBot[Math.floor(Math.random() * optionBot.length)];
+let result = false;
+document.querySelector("#bot").innerHTML= `BOT: ${bot}`;
+document.querySelector("#player").innerHTML= `PLAYER: ${player}`;
+if (player === bot) {
+  document.querySelector("#result").innerHTML= "Empate";
+  result = true;
+}
+
+if (player === "Tijera" && bot === "Papel") {
+  document.querySelector("#result").innerHTML = "Has ganado";
+  result = true;
+} else if(player === "Papel" && bot === "Tijera"){
+  document.querySelector("#result").innerHTML= "Has perdido";
+}  
+
+
+if (player === "Papel" && bot === "Piedra") {
+  document.querySelector("#result").innerHTML = "Has ganado";
+  result = true;
+} else if(player === "Piedra" && bot === "Papel") {
+  document.querySelector("#result").innerHTML= "Has perdido";
+}  
+
+
+if (player === "Piedra" && bot === "Tijera") {
+  document.querySelector("#result").innerHTML = "Has ganado";
+  result = true;
+} else if(player === "Tijera" && bot === "Piedra"){
+  document.querySelector("#result").innerHTML= "Has perdido";
+}  
+
+
