b27a22ea815ac8a77bd2f4b4a22dc2b7ace01477
Xipxap11 <davidplanaquerol@gmail.com>
2026-05-18 17:34:29 +0000

    Buenas practicas

--- src/app.js
+++ src/app.js
@@ -5,38 +5,44 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-let optionBot = ["Piedra", "Papel", "Tijera"]
-let player = window.prompt("Elige: Piedra, papel, tijera");
-let bot = optionBot[Math.floor(Math.random() * optionBot.length)];
-let result = false;
-document.querySelector("#bot").innerHTML= `BOT: ${bot}`;
-document.querySelector("#player").innerHTML= `PLAYER: ${player}`;
-if (player === bot) {
-  document.querySelector("#result").innerHTML= "Empate";
-  result = true;
+const optionBot = ["Piedra", "Papel", "Tijera"]
+const player = window.prompt("Elige: Piedra, papel, tijera");
+const bot = optionBot[Math.floor(Math.random() * optionBot.length)];
+
+function checkWin(player, bot) {
+  if (player === bot) {
+    return "Empate";
+  }
+  
+  if (player === "Tijera" && bot === "Papel") {
+    return "Has ganado";
+  } 
+  
+  if(player === "Papel" && bot === "Tijera"){
+    return "Has perdido";
+  }  
+  
+  if (player === "Papel" && bot === "Piedra") {
+    return "Has ganado";
+  } 
+  
+  if(player === "Piedra" && bot === "Papel") {
+    return "Has perdido";
+  }  
+  
+  if (player === "Piedra" && bot === "Tijera") {
+    return "Has ganado";
+  } 
+  
+  if(player === "Tijera" && bot === "Piedra"){
+    return "Has perdido";
+  }  
 }
 
-if (player === "Tijera" && bot === "Papel") {
-  document.querySelector("#result").innerHTML = "Has ganado";
-  result = true;
-} else if(player === "Papel" && bot === "Tijera"){
-  document.querySelector("#result").innerHTML= "Has perdido";
-}  
+function render(bot, player) {
+  document.querySelector("#bot").innerHTML= `BOT: ${bot}`;
+  document.querySelector("#player").innerHTML= `PLAYER: ${player}`;
+  document.querySelector("#result").innerHTML = checkWin(player, bot);
+}
 
-
-if (player === "Papel" && bot === "Piedra") {
-  document.querySelector("#result").innerHTML = "Has ganado";
-  result = true;
-} else if(player === "Piedra" && bot === "Papel") {
-  document.querySelector("#result").innerHTML= "Has perdido";
-}  
-
-
-if (player === "Piedra" && bot === "Tijera") {
-  document.querySelector("#result").innerHTML = "Has ganado";
-  result = true;
-} else if(player === "Tijera" && bot === "Piedra"){
-  document.querySelector("#result").innerHTML= "Has perdido";
-}  
-
-
+render(bot,player);
